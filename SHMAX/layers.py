5b47a459f3e11b6c7598d59c72f22c27e4b53cdd
hamza <hamza.keurti@gmail.com>
2019-11-21 16:55:16 +0800

    Remove unused class

--- SHMAX/layers.py
+++ SHMAX/layers.py
@@ -4,81 +4,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from numpy.lib.stride_tricks import as_strided
-from sklearn.decomposition import DictionaryLearning,MiniBatchDictionaryLearning,SparseCoder
+from sklearn.decomposition import DictionaryLearning, MiniBatchDictionaryLearning, SparseCoder
+from patcher import Patcher
 
 import spams
 
+WIDTH_INTERVAL = 'width_interval'
+HEIGHT_INTERVAL = 'height_interval'
+
+
 def to_windows(input, kernel_size, step_size=1):
     out_shape = input.shape[:2]
-    out_shape += ((input.shape[2] - kernel_size)//step_size +1, (input.shape[3] - kernel_size)//step_size +1) + (kernel_size,kernel_size)
-    input_srides = input.strides 
-    strides = input_srides[:2] + tuple(np.array(input.strides[-2:])*step_size) + input_srides[-2:]
-    return as_strided(input, shape = out_shape, strides = strides,writeable=False)
-
-
-class SparseLayer(nn.Module): # Convolution-like
-    def __init__(self,input_channels,output_channels,filter_size,batch_size=500):
-        super(SparseLayer,self).__init__()
-        self.input_channels = input_channels
-        self.output_channels = output_channels
-        self.dictionary_size = output_channels
-        self.filter_size = filter_size
-        self.pad = self.filter_size // 2
-        self.n_features = self.filter_size**2 * self.input_channels
-        self.batch_size = batch_size
-        # if isinstance(self.filter_size,int): 
-        # else:
-        #     self.n_features = 1
-        #     for k in self.filter_size:
-        #         self.n_features *= k
-        
-        self.dictionary = DictionaryLearning(n_components = self.dictionary_size)
-    # Actually a reconstruction
-    def forward(self,input):
-        # code : (batch_size, dictionary_size)
-        coder = SparseCoder(dictionary = self.dictionary.components_)
-        padded = np.pad(input,((0,0),(0,0),(self.pad,self.pad),(self.pad,self.pad)),mode='constant')
-        
-        windowed = to_windows(padded,kernel_size=self.filter_size)
-        del padded
-
-        reshaped = windowed.reshape(-1,self.n_features)
-        del windowed
-
-        code = coder.transform(reshaped)
-        # code : (Batch_size * height * width, output channels)
-        return torch.as_tensor(np.transpose(code.reshape((input.shape[0],input.shape[2],input.shape[3],self.output_channels)),(0,3,1,2)))
-
-
-    def forward_conv(self,input):
-        weights = torch.as_tensor(self.dictionary.components_.reshape(self.output_channels,self.input_channels,self.filter_size,self.filter_size))
-        
-        padded = np.pad(input,((0,0),(0,0),(self.pad,self.pad),(self.pad,self.pad)),mode='constant')
-
-        return F.conv2d(torch.as_tensor(padded),weights)
-
-    def train_dictionary(self,input):
-        # input : (batch_size, channels, height, width)
-        patches = self.extract_patches(input)
-        self.dictionary.fit(patches)
-        # self.dictionary_trainer.partial_fit(reshaped)
-
-    def extract_patches(self,input):
-        padded = np.pad(input,((0,0),(0,0),(self.pad,self.pad),(self.pad,self.pad)),mode='constant')
-        
-        windowed = to_windows(padded,kernel_size=self.filter_size)
-        del padded
-
-        reshaped = windowed.reshape(-1,self.n_features)
-        del windowed
-        
-        indx = np.random.randint(reshaped.shape[0],size=self.batch_size)
-        return reshaped[indx]
+    out_shape += ((input.shape[2] - kernel_size)//step_size + 1,
+                  (input.shape[3] - kernel_size)//step_size + 1) + (kernel_size, kernel_size)
+    input_srides = input.strides
+    strides = input_srides[:2] + \
+        tuple(np.array(input.strides[-2:])*step_size) + input_srides[-2:]
+    return as_strided(input, shape=out_shape, strides=strides, writeable=False)
 
 
 class SLayer(nn.Module):
-    def __init__(self,input_channels,output_channels,filter_size,batch_size=500,param={}):
-        super(SLayer,self).__init__()
+    def __init__(self, input_channels, output_channels, filter_size, batch_size=500, param={}, layer_config=None):
+        super(SLayer, self).__init__()
         self.input_channels = input_channels
         self.output_channels = output_channels
         self.dictionary_size = output_channels
@@ -90,88 +37,60 @@ class SLayer(nn.Module):
         self.model = None
         self.param = param
         self.param['K'] = self.dictionary_size
+        width_interval, height_interval = None, None
+        if layer_config:
+            width_interval = layer_config.get(width_interval, None)
+            height_interval = layer_config.get(height_interval, None)
+        self.patcher = Patcher(self.batch_size, self.filter_size,width_interval=width_interval,height_interval=height_interval)
 
-
-    def train(self,input):
-        # padded = np.pad(input,((0,0),(0,0),(self.pad,self.pad),(self.pad,self.pad)),mode='constant')
-        
-        # windowed = to_windows(padded,kernel_size=self.filter_size)
-        # del padded
-
-        # reshaped = windowed.reshape(-1,self.n_features)
-        # del windowed
-
-        # patches = self.extract_patches(reshaped)
+    def train(self, input):
         D = self.train_dictionary(input)
+        self.dictionary = torch.tensor(D.T.reshape(
+            self.output_channels, self.input_channels, self.filter_size, self.filter_size),dtype=torch.float)
 
-        self.dictionary = torch.tensor(D.T.reshape(self.output_channels,self.input_channels,self.filter_size,self.filter_size))
+    def forward_conv(self, input):
+        padded = F.pad(input, (self.pad, self.pad, self.pad, self.pad))
+        return F.conv2d(padded, self.dictionary)
 
-    def forward_conv(self,input):        
-        padded = F.pad(input,(self.pad,self.pad,self.pad,self.pad))
-        return F.conv2d(padded,self.dictionary)
-
-    def backward_conv(self,feature_map):
-        backward_dictionary = self.dictionary.permute(1,0,2,3)
-        padded = F.pad(feature_map,(self.pad,self.pad,self.pad,self.pad))
-        return F.conv2d(padded,backward_dictionary)
-
+    def backward_conv(self, feature_map):
+        backward_dictionary = self.dictionary.permute(1, 0, 2, 3)
+        padded = F.pad(feature_map, (self.pad, self.pad, self.pad, self.pad))
+        return F.conv2d(padded, backward_dictionary)
 
     '''
     Correlates the input image to a learned dictionary
     (Experimental)
     '''
-    def forward_corr(self,input):
-        padded = F.pad(input,(self.pad,self.pad,self.pad,self.pad))
-        return F.conv2d(padded,self.dictionary.flip((-1,-2)))
 
+    def forward_corr(self, input):
+        padded = F.pad(input, (self.pad, self.pad, self.pad, self.pad))
+        return F.conv2d(padded, self.dictionary.flip((-1, -2)))
 
     '''
     Correlates the obtained feature map (code) to the learned dictionary
     (Experimental)
     '''
-    def backward_corr(self,feature_map):
-        backward_dictionary = self.dictionary.permute(1,0,2,3) # permute channels for output / input
-        padded = F.pad(feature_map,(self.pad,self.pad,self.pad,self.pad))
-        return F.conv2d(padded,backward_dictionary.flip((-1,-2))) # Flip for correlation instead of convolution
 
+    def backward_corr(self, feature_map):
+        backward_dictionary = self.dictionary.permute(
+            1, 0, 2, 3)  # permute channels for output / input
+        padded = F.pad(feature_map, (self.pad, self.pad, self.pad, self.pad))
+        # Flip for correlation instead of convolution
+        return F.conv2d(padded, backward_dictionary.flip((-1, -2)))
 
-    def train_dictionary(self,input):
+    def train_dictionary(self, input):
         # input : (batch_size, channels, height, width)
-        patches = self.extract_patches(input)
+        patches = self.patcher.extract_patches(input)
         X = patches.T
-        X = X - np.tile(np.mean(X,0),(X.shape[0],1))
-        X = np.asfortranarray(X / np.tile(np.sqrt((X * X).sum(axis=0)),(X.shape[0],1)),dtype = float)
+        X = X - np.tile(np.mean(X, 0), (X.shape[0], 1))
+        X = np.asfortranarray(
+            X / np.tile(np.sqrt((X * X).sum(axis=0)), (X.shape[0], 1)), dtype=float)
         if self.model:
-            (D,self.model) = spams.trainDL(X,return_model = True,model = self.model,**self.param)
+            (D, self.model) = spams.trainDL(
+                X, return_model=True, model=self.model, **self.param)
             self.param['D'] = D
         else:
-            (D,self.model) = spams.trainDL(X,return_model = True,**self.param)
+            (D, self.model) = spams.trainDL(X, return_model=True, **self.param)
             self.param['D'] = D
         return D
 
-        # self.dictionary_trainer.partial_fit(reshaped)
-
-    def extract_patches(self,input):
-        batch_index = np.random.randint(input.shape[0],size=self.batch_size)
-        # Patches will be selected from index as upper left corner
-        height_index = np.random.randint(input.shape[-2] - self.filter_size + 1, size=self.batch_size)
-        width_index = np.random.randint(input.shape[-1] - self.filter_size + 1, size=self.batch_size)
-
-        patches = np.zeros((self.batch_size,input.shape[1],self.filter_size,self.filter_size))
-        for i in range(self.batch_size):
-            b = batch_index[i]
-            h = height_index[i]
-            w = width_index[i]
-            f = self.filter_size
-            patches[i] = input[b,:,h:h+f,w:w+f]
-
-        return patches.reshape(self.batch_size,-1)
-
-        # # padded = np.pad(input,((0,0),(0,0),(self.pad,self.pad),(self.pad,self.pad)),mode='constant')
-        # windowed = to_windows(input,kernel_size=self.filter_size)
-
-        # reshaped = windowed.reshape(-1,self.n_features)
-        # del windowed
-        
-        # indx = np.random.randint(reshaped.shape[0],size=self.batch_size)
-        # return reshaped[indx]
