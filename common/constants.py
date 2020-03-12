4b29d190bb1c8d0790b90168d422e9a4f3e714ca
hamza <hamza.keurti@gmail.com>
2020-03-12 18:42:46 +0800

    constants file for json management

---
+++ common/constants.py
@@ -0,0 +1,51 @@
+# TRAINING--------------
+TRAINING = 'training'
+MAX_ITERATIONS = "max_iterations"
+N_SUBITERATIONS = "n_subiterations"
+SAVE_RATE = "save_rate"
+SAVE_DIR = "save_dir"
+
+# DATA
+DATA = "data"
+# CONFIG KEYS------
+BATCH_SIZE = 'batch_size'
+DIRECTORY = 'directory'
+DATASET = 'dataset'
+
+# MUSICNET----------
+MUSICNET_DIRECTORY = '/data/valentin/music-learning/musicnet'
+DOWNLOAD = 'download'       
+JITTER = 'jitter'
+PITCH_SHIFT = 'pitch_shift',
+WINDOW = 'window'
+SR = 'sampling_rate'
+# --------------------
+
+# MUSICNET COCHLEAGRAMS-----
+MUSICNET_COCH_DIRECTORY = '/data/hamza/datasets/musicnet/cochleagrams/folder1'
+
+# SPEECH COCHLEAGRAMS-------
+SPEECH_DIRECTORY = '/home/valentin/data/twopaths/pt/fma_large_cgrams'
+
+# MODEL--------------------
+MODEL = 'model'
+DEVICE = 'device'
+## SHMAX------------------
+BATCH_SIZE = 'batch_size'
+POOL_SIZE = 'pool_size'
+POOL_STRIDES = 'pool_strides'
+FILTER_SIZE = 'filter_size'
+N_LAYERS = 'n_layers'
+CHANNELS = 'channels'
+SPAMS_PARAM = 'spams_param'
+N_SUBITERATIONS = 'n_subiterations'
+INCREMENT_RATE = 'increment_rate'
+LAYERS_CONFIG = 'layers_config'
+CHANNELS_IN = 'channels_in'
+## LocalSHMAX---------------
+BAND_INDICES = "band_indices"
+N_BANDS = "n_bands"
+DICT_SIZE = "dict_size"
+FILTER_SHAPE = "filters_shape"
+PATCH_BATCH = 'patch_batch'
+MAX_PATCHES = "max_patches"
