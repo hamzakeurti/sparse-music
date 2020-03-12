590da8695bdcf12bf157ec8d63f46a725913c366
hamza <hamza.keurti@gmail.com>
2020-03-12 19:08:20 +0800

    local shmax (band wise sparse coding) model

--- localmlp/layers.py
+++ localmlp/layers.py
@@ -2,8 +2,14 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import os
 import spams
+import pickle
+
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+from sparse_music.common import constants as const
 
 
 class BandBlock(nn.Module):
@@ -13,71 +19,120 @@ class BandBlock(nn.Module):
         self.dict_tensor = None
         self.dict_size = dict_size
         self.filters_shape = filters_shape  # H,W
-        self.param = spams_param
+        self.spams_param = spams_param
         self.device = device
-        self.param["K"] = self.dict_size
+        self.spams_param["K"] = self.dict_size
         self.channels_out = dict_size
         self.channels_in = channels_in
-        self.model = None
+        self.savable = ["dict", "dict_tensor", "dict_size",
+                        "filters_shape", "spams_param", "device", "channels_out", "channels_in"]
 
     def train(self, patches, standardize=False):
         # call spams train memory
-        if len(data.shape > 2):
-            data = patches.reshape(data.shape[0], -1)
-        patches = patches.T
+        if len(patches.shape) > 2:
+            patches = patches.reshape(patches.shape[0], -1)
         patches = np.asfortranarray(patches)
-        self.dict, self.model = learn_dictionary(
-            patches, self.param, standardize_input=standardize)
+        self.dict = learn_dictionary(
+            patches, self.spams_param, standardize_input=standardize)
         self.dict_tensor = torch.tensor(
-            self.dict.T.reshape((self.channels_out, self.channels_in)+self.filters_shape)).to(device)
+            self.dict.T.reshape((self.channels_out, self.channels_in)+tuple(self.filters_shape))).to(self.device)
 
     def forward(self, input):
         # do conv
         return F.conv2d(input=input, weight=self.dict_tensor)
 
+    def save(self, filename):
+        data = {}
+        for attr in self.__dict__:
+            if attr in self.savable:
+                data[attr] = self.__dict__[attr]
+        with open(filename, "wb") as f:
+            pickle.dump(data, f)
 
-class LocalMlpLayer(nn.Module):
-    def __init__(self, filters_shape, dict_size, channels_in=1, batch_size=50, band_indices=None, band_blocks=None, n_bands=None, spams_param=None, device='cpu'):
-        super(LocalMlpLayer, self).__init__()
+
+class LocalSHMAX(nn.Module):
+    def __init__(self, filters_shape, dict_size, channels_in=1, patch_batch=50, band_indices=None, band_blocks=None, n_bands=None, spams_param=None, device='cpu'):
+        super(LocalSHMAX, self).__init__()
         self.dict_size = dict_size
         self.filters_shape = filters_shape
-        self.batch_size = batch_size
-
-        # Allow for self self specified band locations, to overlap bands if wanted
+        self.channels_in = channels_in
+        self.patch_batch = patch_batch
+        self.band_blocks = None
+        self.spams_param = spams_param
+        # Allow for self specified band locations, to overlap bands if wanted
         if band_indices:
             self.band_indices = band_indices
         elif band_blocks:
             n_bands = len(band_blocks)
-            self.band_blocks = torch.ModuleList([list(band_blocks)])
+            self.band_blocks = nn.ModuleList([list(band_blocks)])
             self.band_indices = [i*filters_shape[0] for i in range(n_bands)]
         elif n_bands:
             self.band_indices = [i*filters_shape[0] for i in range(n_bands)]
 
         self.n_bands = len(band_indices)
-
+        self.device = device
         if not self.band_blocks:
-            self.band_blocks = torch.ModuleList(
+            self.band_blocks = nn.ModuleList(
                 [BandBlock(spams_param.copy(), filters_shape, dict_size, channels_in, device) for i in range(self.n_bands)])
 
         self.patches = [[] for i in range(self.n_bands)]
+        self.savable = ["dict_size" ,"filters_shape","spams_param" ,"patch_batch" ,"band_indices" ,"n_bands" ,"device"]
+
+    @classmethod
+    def from_config(cls, config):
+        filters_shape = config.get(const.FILTER_SHAPE, None)
+        channels_in = config.get(const.CHANNELS_IN, 1)
+        dict_size = config.get(const.DICT_SIZE, None)
+        patch_batch = config.get(const.PATCH_BATCH, None)
+        band_indices = config.get(const.BAND_INDICES, None)
+        n_bands = config.get(const.N_BANDS, None)
+        spams_param = config.get(const.SPAMS_PARAM, None)
+        device = config.get(const.DEVICE, None)
+        return cls(filters_shape, dict_size, channels_in=channels_in, patch_batch=patch_batch, band_indices=band_indices, n_bands=n_bands, spams_param=spams_param, device=device)
+
+
+    @classmethod
+    def from_saved(cls,directory):
+        if directory.endswith('.p'):
+            directory = directory[:-2]
+        with open(f'{directory}model.p','rb') as f:
+            data = pickle.load(f)
+        filters_shape = data.get(const.FILTER_SHAPE)
+        dict_size = data.get(const.DICT_SIZE)
+        channels_in = data.get(const.CHANNELS_IN)
+        patch_batch = data.get(const.PATCH_BATCH)
+        band_indices = data.get(const.BAND_INDICES)
+        spams_param = data.get(const.SPAMS_PARAM)
+        device = data.get(const.DEVICE)
+        model = cls(filters_shape, dict_size, channels_in=channels_in, patch_batch=patch_batch, band_indices=band_indices, spams_param=spams_param, device=device)
+        for b in range(model.n_bands):
+            with open(f'{directory}{b}.p','rb') as f:
+                data = pickle.load(f)
+                model.band_blocks[b].dict = data['dict']
+                model.band_blocks[b].dict_tensor = data['dict_tensor']
+        return model
 
     def extract_patches(self, input):
-        self.patches = [[] for i in range(self.n_bands)]
+        if len(input.shape) <4:
+            input = torch.unsqueeze(input, 1)
         for i in range(len(self.band_blocks)):
-            self.patches[i].append(patch(input, batch_size=self.batch_size, filter_height=self.filters_shape[0],
-                                    filter_width=self.filters_shape[1], height_interval=[self.band_indices[i], self.band_indices[i]+1]))
+            # self.patches is a list of per band patches, for each band, it contains a list of batches of patches to be concatenated at training time
+            self.patches[i].append(patch(input, patch_batch=self.patch_batch, filter_height=self.filters_shape[0],
+                                         filter_width=self.filters_shape[1], height_interval=[self.band_indices[i], self.band_indices[i]+1]))
 
-    def train(self, data):
+    def train(self):
         # Patch here?
         self.patches = [np.concatenate(p, axis=0) for p in self.patches]
         for i, band in enumerate(self.band_blocks):
             # Need to make sure we're using patched data
+            print(f'Training band {i}/{self.n_bands}')
             band.train(self.patches[i])
         del self.patches
 
     def forward(self, input):
         output = []
-        input = torch.tensor(input).to(self.device)
+        if len(input.shape) <4:
+            input = torch.unsqueeze(input, 1)
         for i, band in enumerate(self.band_blocks):
             output.append(band.forward(
                 input[:, :, self.band_indices[i]:self.band_indices[i]+self.filters_shape[0], :]))
@@ -85,22 +140,43 @@ class LocalMlpLayer(nn.Module):
         output = torch.cat(output, dim=-2)
         return output
 
+    def save(self, directory):
+        # remove extension if present
+        if directory.endswith('.p'):
+            directory = directory[:-2]
+        
+        # create directory if not existing
+        if not os.path.exists(os.path.dirname(directory)):
+            os.mkdir(os.path.dirname(directory))
+        
+        # save model's attributes
+        data = {}
+        for attr in self.__dict__:
+            if attr in self.savable:
+                data[attr] = self.__dict__[attr]
+        with open(f'{directory}model.p', "wb") as f:
+            pickle.dump(data, f)
+        
+        # save composing band's dictionaries
+        for b in range(self.n_bands):
+            self.band_blocks[b].save(f'{directory}{b}.p')
+        data = {}
 
-def patch(input, batch_size, filter_height, filter_width, width_interval=None, height_interval=None):
+
+def patch(input, patch_batch, filter_height, filter_width, width_interval=None, height_interval=None):
     if not height_interval:
         height_interval = (0, input.shape[-2] - filter_height + 1)
     if not width_interval:
         width_interval = (0, input.shape[-1] - filter_width + 1)
 
     height_index = np.random.randint(
-        low=height_interval[0], high=height_interval[1], size=batch_size)
+        low=height_interval[0], high=height_interval[1], size=patch_batch)
     width_index = np.random.randint(
-        low=width_interval[0], high=width_interval[1], size=batch_size)
-    batch_index = np.random.randint(input.shape[0], size=batch_size)
-
+        low=width_interval[0], high=width_interval[1], size=patch_batch)
+    batch_index = np.random.randint(input.shape[0], size=patch_batch)
     patches = np.zeros(
-        (batch_size, input.shape[1], filter_height, filter_width))
-    for i in range(batch_size):
+        (patch_batch, input.shape[1], filter_height, filter_width))
+    for i in range(patch_batch):
         b = batch_index[i]
         h = height_index[i]
         w = width_index[i]
@@ -111,19 +187,14 @@ def patch(input, batch_size, filter_height, filter_width, width_interval=None, h
     return patches
 
 
-def learn_dictionary(patches, param, model=None, standardize_input=True):
+def learn_dictionary(patches, spams_param, model=None, standardize_input=True):
     X = patches.T
     if standardize_input:
         X = standardize(X)
     X = np.asfortranarray(X, dtype=float)
-    if model:
-        (D, model) = spams.trainDL(
-            X, return_model=True, model=model, **param)
-        param['D'] = D
-    else:
-        (D, model) = spams.trainDL_Memory(X, return_model=True, **param)
-        param['D'] = D
-    return D, model
+    D = spams.trainDL(X, model=model, **spams_param)
+    spams_param['D'] = D
+    return D
 
 
 def standardize(X):
