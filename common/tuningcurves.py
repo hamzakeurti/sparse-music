118deaa7a6774b485106f955db90954e6baf6ec9
Hamza Keurti <hamza.keurti@gmail.com>
2020-03-15 17:05:52 +0100

    Added plot histogram function

--- common/tuningcurves.py
+++ common/tuningcurves.py
@@ -1,5 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+harmonic_ratios = [1/2,1/3,2/5,2/3,3/4,3/5,4/5]
+harmonic_ratios2 = [2,3,5/2,3/2,4/3,5/3,5/4]
 
 def gaussian(window, sigma):
     return np.exp(-((np.arange(window) - window//2) ** 2)/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
@@ -37,3 +40,21 @@ def consecutive_peaks_ratios(peaks_arr, freqs_arr, min_n_peaks=2, max_n_peaks=4)
             for j in range(n_peaks-1):
                 ratios.append(n_peaked[n_peaks*i+j]/n_peaked[n_peaks*i+j+1])
     return ratios
+
+def plot_ratios_histogram(ratios,bins=25,alpha=0.5,save_file=None):
+    if ratios>1:
+        xharmonics = harmonic_ratios2
+    else :
+        xharmonics = harmonic_ratios
+    plt.figure()
+    ret = plt.hist(ratios,bins=bins,alpha = alpha)
+    plt.xlabel('Consecutive peaks ratios')
+    plt.ylabel('Count')
+    for xc in xharmonics[:-1]:
+        plt.axvline(x=xc,linestyle = '--',alpha = 0.8)
+    plt.axvline(x=xharmonics[-1],linestyle = '--', alpha = 0.8,label='harmonic ratios')
+    plt.legend()
+    if save_file:
+        plt.savefig(save_file)
+    plt.close()
+    return ret
