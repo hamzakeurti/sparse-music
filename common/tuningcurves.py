318b4357441193b6f052ba79ae9e326a659610e5
hamza <hamza.keurti@gmail.com>
2020-05-10 17:45:56 +0800

    Adjustment on peaks function

--- common/tuningcurves.py
+++ common/tuningcurves.py
@@ -55,18 +55,21 @@ def peaks_gradient(arr, n, threshold=None, percentile=80):
     return out_arr
 
 
-def peaks_bandwidth(arr, n,level = 1.1):
+def peaks_bandwidth(arr, n,m,level = 1.1):
     out_arr = np.zeros(arr.shape,dtype=bool)
     out_arr[..., n+1:-(n+1)] = True
-    for i in range(0, n//2):
+
+    for i in range(1, m):
         out_arr[..., n+1:-(n+1)] &= (arr[..., n+1:-(n+1)] > arr[..., n+1-i:-(n+1)-i]) # increasing to the left of the peak      
+
         out_arr[..., n+1:-(n+1)] &= (arr[..., n+1:-(n+1)] > arr[..., n+1+i:-(n+1)+i])
         
-    for i in range(n//2,n):
+    for i in range(m,n):
         out_arr[..., n+1:-(n+1)] &= (arr[..., n+1:-(n+1)] > level*arr[..., n+1-i:-(n+1)-i]) # increasing to the left of the peak      
         out_arr[..., n+1:-(n+1)] &= (arr[..., n+1:-(n+1)] > level*arr[..., n+1+i:-(n+1)+i])
 
     return out_arr
+
 
 
 def consecutive_peaks_ratios(peaks_arr, freqs_arr, min_n_peaks=2, max_n_peaks=4):
